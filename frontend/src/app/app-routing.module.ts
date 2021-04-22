4824ea7d33870116ad5f532082564963da78d4e9
retsaftu <57732971+retsaftu@users.noreply.github.com>
2021-04-22 22:37:42 +0600

    Add files via upload

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./home/home.component";
+import { ListComponent } from "./list/list.component";
+import { P404Component } from "./p404/p404.component";
+import { ContactComponent } from "./contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "list", component: ListComponent},
+  {path: "contact", component: ContactComponent},
+  {path: "**", component: P404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
